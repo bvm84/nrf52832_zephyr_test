f4525fbce614339f0661eb6ddb231142870ab7bf
Brendel Vadim <brendel.vadim@gmail.com>
2022-06-02 14:26:07 +0300

    first commit

---
+++ build/image_preload.cmake
@@ -0,0 +1,5 @@
+# Generated file that can be used to preload variant images
+set(CACHED_CONF_FILE C:/code/nrf52832_zephyr_tests/prj.conf CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE C:/code/nrf52832_zephyr_tests/nrf52dk_nrf52832.overlay CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION NONE CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON c:\code\v1.9.1\toolchain\opt\bin\python.exe CACHE INTERNAL "NCS child image controlled")
---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,13 @@
+empty_file.o: C:/code/v1.9.1/zephyr/misc/empty_file.c \
+ C:/code/v1.9.1/zephyr/boards/arm/nrf52dk_nrf52832/nrf52dk_nrf52832.dts \
+ C:/code/v1.9.1/zephyr/dts/arm/nordic/nrf52832_qfaa.dtsi \
+ C:/code/v1.9.1/zephyr/dts/common/mem.h \
+ C:/code/v1.9.1/zephyr/dts/arm/nordic/nrf52832.dtsi \
+ C:/code/v1.9.1/zephyr/dts/arm/armv7-m.dtsi \
+ C:/code/v1.9.1/zephyr/dts/common/skeleton.dtsi \
+ C:/code/v1.9.1/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ C:/code/v1.9.1/zephyr/include/dt-bindings/i2c/i2c.h \
+ C:/code/v1.9.1/zephyr/include/dt-bindings/gpio/gpio.h \
+ C:/code/v1.9.1/zephyr/include/dt-bindings/pinctrl/nrf-pinctrl.h \
+ C:/code/v1.9.1/zephyr/dts/arm/nordic/override.dtsi \
+ C:/code/nrf52832_zephyr_tests/nrf52dk_nrf52832.overlay
